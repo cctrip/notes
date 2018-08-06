f2a3f6dab9fd8482deebbfdd4dfe05bbc1d82121
Code.CC <zcc0388@gmail.com>
2018-08-06 16:47:51 +0800

    Add ansible roles that zookeeper

---
+++ intelligent/Ansible/case/roles/zookeeper/templates/zk.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+source /etc/profile
+
+zkServer={{ work_dir }}/zookeeper-{{ zk_version }}/bin/zkServer.sh
+zkCfg=/etc/zookeeper/zoo.cfg
+
+function start(){
+	PID=$(jps | grep -i QuorumPeerMain | awk '{print $1}')
+	jps | grep -i QuorumPeerMain >/dev/null
+	if [ $? -ne 0 ];then
+		${zkServer} start ${zkCfg} &
+	else
+		echo "Zookeeper is running (pid $PID)"
+	fi
+}
+
+function stop(){
+	PID=$(jps | grep -i QuorumPeerMain | awk '{print $1}')
+	[ -n "${PID}" ] && ${zkServer} stop ${zkCfg}
+	sleep 5
+	jps | grep -i QuorumPeerMain >/dev/null
+	if [ $? -ne 0 ];then
+		echo "Zookeeper is stoped"
+	else
+		kill -9 ${PID}
+	fi
+}
+
+function status(){
+	PID=$(jps | grep -i QuorumPeerMain | awk '{print $1}')
+	[ -n "${PID}" ] && echo "Zookeeper is running (pid ${PID})" || echo "Zookeeper is stoped"
+}
+
+case $1 in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  status)
+    status
+    ;;
+  restart)
+    stop
+    sleep 3
+    start
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop|restart|status}"
+        echo "Example: sh $0 zk.sh status"
+esac
